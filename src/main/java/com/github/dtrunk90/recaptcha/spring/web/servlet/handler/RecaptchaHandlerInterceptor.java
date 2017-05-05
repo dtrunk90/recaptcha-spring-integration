a267087c8fae1056e1780f5063161d2a1e262ab1
Danny Trunk <dtrunk90@gmail.com>
2017-05-05 19:02:31 +0200

    Add recaptchaSiteKey model attribute not if its a redirect

--- src/main/java/com/github/dtrunk90/recaptcha/spring/web/servlet/handler/RecaptchaHandlerInterceptor.java
+++ src/main/java/com/github/dtrunk90/recaptcha/spring/web/servlet/handler/RecaptchaHandlerInterceptor.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.view.RedirectView;
 
 public class RecaptchaHandlerInterceptor extends HandlerInterceptorAdapter {
 
@@ -16,7 +17,7 @@ public class RecaptchaHandlerInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
-		if (modelAndView != null) {
+		if (modelAndView != null && modelAndView.hasView() && !(modelAndView.getView() instanceof RedirectView) && !modelAndView.getViewName().startsWith("redirect:")) {
 			modelAndView.addObject(RECAPTCHA_SITE_KEY_ATTRIBUTE_NAME, recaptchaSiteKey);
 		}
 	}
